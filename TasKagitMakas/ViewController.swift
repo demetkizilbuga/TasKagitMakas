7a19f8e1dbfc1ffd16c05ab4919278d8a6b4ba7c
Demet KIZILBUĞA <demetkizilbuga@gmail.com>
2018-11-11 16:17:44 +0300

    Initial Commit

---
+++ TasKagitMakas/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TasKagitMakas
+//
+//  Created by Demet KIZILBUĞA on 11.11.2018.
+//  Copyright © 2018 Demet KIZILBUĞA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
